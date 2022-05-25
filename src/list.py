472e8b96218726290145725cf0840412df925135
Goko6 <gokberk.arslan2016@gmail.com>
2022-05-25 21:18:41 +0300

    list types done

--- src/list.py
+++ src/list.py
@@ -1,11 +1,32 @@
-def list_types():
+def list_types(outputName):
 
-    pass
+    ## Get the type names from system catalog file
+    ## And insert them into the set record_types
+    syscatf = open("./db/syscat", "r")
+    lines = syscatf.readlines()
+    record_types = set()
+    for i in range(len(lines)):
+        if lines[i][20:40] != "systemCatalog".ljust(20):
+            record_types.add(lines[i][20:40])
+    syscatf.close()
+
+    ## If there is no type, the operation is unsuccessful
+    if len(record_types) == 0:
+        return 1
+
+    ## Printing the types to the output file
+    outfile = open(outputName, "a")
+    for i in record_types:
+        outfile.write(i.replace(" ","") + "\n")
+
+    return 0
 
 def list_records():
 
     pass
 
 if __name__=="__main__":
+
+    list_types("output.txt")
 
     pass
